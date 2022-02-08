b1727434fb70c48f5779b358c006e3e4fc8f1ccb
DirtyMills <93839074+DirtyMills@users.noreply.github.com>
2022-02-08 11:10:41 -0500

    Added rectangle class with 6 methods

---
+++ AaronMillerAssignment2PROG2070/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AaronMillerAssignment2PROG2070
+{
+    public class Rectangle
+    {
+        private int Length;
+        private int Width;
+
+        public Rectangle ()
+        {
+            Length = 1;
+            Width = 1;
+        }
+
+        public Rectangle (int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public int GetLength()
+        {
+            return this.Length;
+        }  
+        public int SetLength(int length)
+        {
+            this.Length = length;
+            return length;
+        }
+        public int GetWidth()
+        {
+            return this.Width;
+        }
+        public int SetWidth(int width)
+        {
+            this.Width = width;
+            return Length;
+        }  
+        public int GetPerimeter()
+        {
+            int perimeter = (this.Width * 2) + (this.Length * 2);
+            return perimeter;
+        }
+        public int GetArea()
+        {
+            int area = this.Width * this.Length;
+            return area;
+        }
+
+    }
+}
